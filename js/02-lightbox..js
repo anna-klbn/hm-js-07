788f027a77b09b1cae4613c160d71f5bcd97ce46
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2023-10-17 22:14:44 +0300

    task-02

--- js/02-lightbox..js
+++ js/02-lightbox..js
@@ -1,4 +1,5 @@
 import { galleryItems } from './gallery-items.js';
+
 const galleryItemsContainer = document.querySelector('.gallery');
 const galleryItemsMarkup = createGalleryItemCardMarkup(galleryItems);
 
@@ -8,20 +9,13 @@ galleryItemsContainer.addEventListener('click', onGalleryItemsContainerClick);
 function createGalleryItemCardMarkup(galleryItems) {
 
     return galleryItems.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+        return `<a class="gallery__item" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${original}"
       alt="${description}"
     />
-  </a>
-</div>`;
+  </a>`;
     }).join('');
 }
-
- 
-
-
 
